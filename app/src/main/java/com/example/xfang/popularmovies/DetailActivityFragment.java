ccf9df82324a96250575a1e8c0bb61c21dca6bf5
Xiaofan <ufa.fan@gmail.com>
2015-07-23 14:23:10 -0700

    DetailActivity displays all information. The layout is also fixed to be somewhat tolerable

--- app/src/main/java/com/example/xfang/popularmovies/DetailActivityFragment.java
+++ app/src/main/java/com/example/xfang/popularmovies/DetailActivityFragment.java
@@ -1,14 +1,19 @@
 package com.example.xfang.popularmovies;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.xfang.popularmovies.model.Movie;
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -16,6 +21,10 @@ import com.example.xfang.popularmovies.model.Movie;
 public class DetailActivityFragment extends Fragment {
 
     TextView mTitleView;
+    ImageView mImageView;
+    TextView mDateView;
+    TextView mRatingView;
+    TextView mPlotView;
     public DetailActivityFragment() {
     }
 
@@ -28,10 +37,26 @@ public class DetailActivityFragment extends Fragment {
         Intent intent = getActivity().getIntent();
 
         mTitleView = (TextView) rootView.findViewById(R.id.detail_title);
+        mImageView = (ImageView) rootView.findViewById(R.id.detail_image);
+        mDateView = (TextView) rootView.findViewById(R.id.detail_date);
+        mRatingView = (TextView) rootView.findViewById(R.id.detail_rating);
+        mPlotView = (TextView) rootView.findViewById(R.id.detail_plot);
+
         if (intent!= null){
             Bundle bundle = intent.getBundleExtra(Movie.EXTRA_MOVIE);
             Movie movie = new Movie(bundle);
+
             mTitleView.setText(movie.title);
+            mDateView.setText(movie.date);
+            mRatingView.setText(Double.toString(movie.rating));
+            mPlotView.setText(movie.plot);
+
+
+            Uri posterUri = movie.getUri("w" + Movie.API_POSTER_SIZE);
+            Picasso.with(getActivity())
+                    .load(posterUri)
+                    .into(mImageView);
+
         }
         return rootView;
     }
